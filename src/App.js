1b0b60f8985f1b343ddd68cdef73b4885d970df9
Anish Reddy Alle <anish.alle11@gmail.com>
2024-06-27 22:00:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This website is under <code> construction </code>, Please check back later!
         </p>
         <a
           className="App-link"
